8e10f77d8b5be6637beefd25e882a6b047d23af3
David Arthur <mumrah@gmail.com>
2017-01-18 22:17:18 -0500

    Only use values with 4 upper bits for PORTD

--- hardware/AFSK.h
+++ hardware/AFSK.h
@@ -11,23 +11,23 @@
 #include "protocol/HDLC.h"
 
 #define SIN_LEN 512
-static const uint8_t sin_table[] PROGMEM =
-{
-    128, 129, 131, 132, 134, 135, 137, 138, 140, 142, 143, 145, 146, 148, 149, 151,
-    152, 154, 155, 157, 158, 160, 162, 163, 165, 166, 167, 169, 170, 172, 173, 175,
-    176, 178, 179, 181, 182, 183, 185, 186, 188, 189, 190, 192, 193, 194, 196, 197,
-    198, 200, 201, 202, 203, 205, 206, 207, 208, 210, 211, 212, 213, 214, 215, 217,
-    218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233,
-    234, 234, 235, 236, 237, 238, 238, 239, 240, 241, 241, 242, 243, 243, 244, 245,
-    245, 246, 246, 247, 248, 248, 249, 249, 250, 250, 250, 251, 251, 252, 252, 252,
-    253, 253, 253, 253, 254, 254, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255,
+static const uint8_t sin_table[] PROGMEM = {
+    // These values have been truncated to the upper 4 bits of PORTD
+    128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 144, 144, 144, 144, 144,
+    144, 144, 144, 144, 144, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160,
+    176, 176, 176, 176, 176, 176, 176, 176, 176, 176, 176, 192, 192, 192, 192, 192,
+    192, 192, 192, 192, 192, 192, 192, 192, 208, 208, 208, 208, 208, 208, 208, 208,
+    208, 208, 208, 208, 208, 208, 224, 224, 224, 224, 224, 224, 224, 224, 224, 224,
+    224, 224, 224, 224, 224, 224, 224, 240, 240, 240, 240, 240, 240, 240, 240, 240,
+    240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240,
+    240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240
 };
 
 inline static uint8_t sinSample(uint16_t i) {
     uint16_t newI = i % (SIN_LEN/2);
     newI = (newI >= (SIN_LEN/4)) ? (SIN_LEN/2 - newI -1) : newI;
     uint8_t sine = pgm_read_byte(&sin_table[newI]);
-    return (i >= (SIN_LEN/2)) ? (255 - sine) : sine;
+    return (i >= (SIN_LEN/2)) ? (256 - sine) : sine;
 }
 
 
@@ -142,7 +142,7 @@ typedef struct Afsk
 
 #define AFSK_DAC_IRQ_START()   do { extern bool hw_afsk_dac_isr; hw_afsk_dac_isr = true; } while (0)
 #define AFSK_DAC_IRQ_STOP()    do { extern bool hw_afsk_dac_isr; hw_afsk_dac_isr = false; } while (0)
-#define AFSK_DAC_INIT()        do { DAC_DDR |= 0xF8; } while (0)
+#define AFSK_DAC_INIT()        do { DAC_DDR |= 0xF0; } while (0)
 
 // Here's some macros for controlling the RX/TX LEDs
 // THE _INIT() functions writes to the DDRB register
